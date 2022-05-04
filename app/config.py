42dab361b6bd62a150d3dccc7022543df1c70041
Olliemint <furywrld5@gmail.com>
2022-05-04 15:55:08 +0300

    files commits

---
+++ app/config.py
@@ -0,0 +1,18 @@
+
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+
+class ProdConfig(Config):
+    
+    pass
+
+
+class DevConfig(Config):
+    
+    
+    DEBUG = True    
